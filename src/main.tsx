2be348abcd4b0f4712a429c50cb02a9d860bf7b0
wolkemann <ohtoristudent@gmail.com>
2022-04-28 21:34:20 +0200

    quick-fix

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalStyle } from "./components/GlobalStyle/GlobalStyle";
 
+document.documentElement.requestFullscreen({ navigationUI: "hide" });
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalStyle />
